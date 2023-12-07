1f778e013081ffa8b0e54999296d0d890ad21263
Roberto Rebolledo <robertorebolledo151@gmail.com>
2023-12-07 10:07:34 +0100

    main

---
+++ src/main/java/com/rebolledonaharro/CursosPuente/CursosPuenteApplication.java
@@ -0,0 +1,13 @@
+package com.rebolledonaharro.CursosPuente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosPuenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosPuenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebolledonaharro/CursosPuente/CursosPuenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebolledonaharro.CursosPuente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosPuenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
